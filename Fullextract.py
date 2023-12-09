e68dfdf8c6f245b318c61aa90b699802efcb5bbb
Théo Armaghan <theo.armaghan@alumni.univ-avignon.fr>
2023-12-09 12:57:45 -0500

    Right comments

--- Fullextract.py
+++ Fullextract.py
@@ -5,6 +5,8 @@ from io import StringIO
 import os
 import re
 
+
+# To find the right country column
 def find_country_column(columns):
     pattern = re.compile(r'\bcountry\b|\bterritory\b|\bcountries\b|\blocation\b', re.IGNORECASE)
     for col in columns:
@@ -12,11 +14,11 @@ def find_country_column(columns):
             return col
     return None
 
+# First extract from wikipedia
 def extract_wikipedia_table_to_csv(url, output_file):
-    # Send a GET request to the URL
+
     response = requests.get(url)
 
-    # Parse the HTML content of the page
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # Find all tables in the HTML
@@ -33,47 +35,42 @@ def extract_wikipedia_table_to_csv(url, output_file):
             print(f"\nTable {i} Preview:")
             print(df_preview.head())
 
-        # Ask the user to choose a table
+        # Ask to choose a table
         table_choice = int(input("\nEnter the number of the table you want to extract: ")) - 1
 
-        # Validate the choice
         if table_choice < 0 or table_choice >= len(tables):
             print("Invalid table number. Exiting.")
             exit(1)
     else:
         table_choice = 0
 
-    # Convert the chosen HTML table to a string
     table_str = str(tables[table_choice])
 
-    # Use StringIO to read the string as a file-like object
     df = pd.read_html(StringIO(table_str))[0]
 
-    # Save the DataFrame to a CSV file
     df.to_csv(output_file, index=False)
 
 
+# Soft Clean (choose a column)
 def clean_date(input_file):
-    # Charger le fichier CSV
+
     df = pd.read_csv(input_file)
 
-    # Afficher les noms de colonnes avec un index
     print("Available columns:")
     for i, col in enumerate(df.columns, 1):
         print(f"{i}. {col}")
 
-    # Identifier automatiquement la colonne du pays
+    # Find the country column
     country_col = find_country_column(df.columns)
     if not country_col:
         country_col = input('Enter the country column name: ')
 
-    # Afficher les 5 premières lignes
     print(df.head())
 
-    # Demande à l'utilisateur de choisir le numéro d'une colonne
+    # Ask to choose a column to keep
     column_index = int(input('Enter the number of the column you want to keep: ')) - 1
 
-    # Vérifier si le numéro de colonne est valide
+    # Check if the column index is valid
     if column_index < 0 or column_index >= len(df.columns):
         print("Invalid column number. Please try again.")
         os.remove('tmp.csv')
@@ -83,21 +80,17 @@ def clean_date(input_file):
 
     result_df = df[[country_col, column_to_keep]]
 
-    # Enregistrer le DataFrame dans un fichier CSV et demander à l'utilisateur de choisir un nom de fichier
+    # Ask for an output file name
     output_file = input('Enter a file name: ')
 
-    #Ajouter .csv à la fin du nom de fichier
     output_file += '.csv'
 
     result_df.to_csv(output_file, index=False)
 
-    #supprimer le fichier tmp.csv
     os.remove('tmp.csv')
 
-# Demande à l'utilisateur de saisir une URL
 url = input('Enter a URL: ')
 
-# Name of the output CSV file
 output_file = 'tmp.csv'
 
 extract_wikipedia_table_to_csv(url, output_file)
